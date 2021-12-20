d6743cc18187db048ecb59c308adbe645f9203f1
Sergio A. Arevalo Soria <sergio.alberto.arevalo.soria@nav.no>
2021-12-20 10:37:04 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# tms-mikrofrontend-template
---
+++ rollup.config.prod.js
@@ -0,0 +1,51 @@
+import babel from "@rollup/plugin-babel";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import replace from "@rollup/plugin-replace";
+import importmap from "@eik/rollup-plugin";
+import json from "@rollup/plugin-json";
+import postcss from "rollup-plugin-postcss";
+import postcssLessLoader from "rollup-plugin-postcss-webpack-alias-less-loader";
+
+const reactUrl = "https://neik.dev.intern.nav.no/npm/@esm-bundle/react/17.0.1/package/index.js";
+const reactDomUrl = "https://neik.dev.intern.nav.no/npm/@esm-bundle/react-dom/17.0.1/package/index.js";
+
+const imports = {
+  react: reactUrl,
+  "react-dom": reactDomUrl,
+};
+
+export default {
+  input: "src/App.js",
+  plugins: [
+    importmap({
+      maps: [{ imports }],
+    }),
+    replace({
+      "process.env.NODE_ENV": JSON.stringify("production"),
+    }),
+    nodeResolve({
+      extensions: [".js"],
+    }),
+    babel({
+      presets: ["@babel/preset-react"],
+    }),
+    commonjs({
+      include: ["node_modules/**"],
+    }),
+    json(),
+    postcss({
+      extract: false,
+      loaders: [
+        postcssLessLoader({
+          nodeModulePath: "./node_modules",
+          aliases: {},
+        }),
+      ],
+    }),
+  ],
+  output: {
+    file: "dist/bundle.esm.js",
+    format: "esm",
+  },
+};
