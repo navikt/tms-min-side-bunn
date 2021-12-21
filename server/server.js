ffbad092be10cb0473d6e076ca4646d41440955c
sasoria <sergio.alberto.arevalo.soria@nav.no>
2021-12-21 14:19:03 +0100

    Importerer path

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 const basePath = "/mikronfrontend";
 const buildPath = path.resolve(__dirname, "../dist");
 const server = express();
